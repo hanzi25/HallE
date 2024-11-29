2c13e9ca03122e1e62bcedb311078ab895d06b4e
hanzi <wanghanziuki@163.com>
2024-11-29 17:18:12 +0800

    Add experiment results to  Readme

--- eval_total2.sh
+++ eval_total2.sh
@@ -1,13 +1,13 @@
-model_path=/raid_sdd/whz/experiments/halle/train/exp5_with_weight_attn_llava_verifier_matrix_minus1_9k_1ep_8bz_4e5
-CUDA_VISIBLE_DEVICES=1 python3 eval/model_controller.py \
-            --model-path $model_path \
+model_path=/raid_sdd/whz/experiments/halle/train/exp5_llava_verifier_vector_minus1_9k_1ep_4bz_3e5
+CUDA_VISIBLE_DEVICES=0 python3 eval/model_controller.py \
+            --model-path /raid_sdd/zzy/experiments/halle/train/exp5_llava_verifier_vector_minus1_9k_1ep_4bz_3e5 \
             --model-version llava_verifier \
             --bf16 True \
             --gt_file_path /raid_sdd/zzy/data/halle/coco/coco2014/annotations/instances_val2014.json \
             --image_path /raid_sdd/zzy/data/halle/coco/coco2014/val2014 \
             --output_folder $model_path/eval
 
-CUDA_VISIBLE_DEVICES=1 python chair.py \
+CUDA_VISIBLE_DEVICES=0 python chair.py \
     --cap_file $model_path/eval/llava_verifier.jsonl \
     --image_id_key image_id \
     --caption_key caption \
--- scripts/v1_5/tune_verifier.sh
+++ scripts/v1_5/tune_verifier.sh
@@ -1,4 +1,4 @@
-deepspeed --include localhost:2,3 --master_port 25431 llava/train/train_verifier.py \
+deepspeed --include localhost:0,1 --master_port 25432 llava/train/train_verifier.py \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path /raid_sdd/whz/model/llava_1_5 \
     --version v1 \
@@ -9,9 +9,10 @@ deepspeed --include localhost:2,3 --master_port 25431 llava/train/train_verifier
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
+    --alpha_type matrix \
     --image_aspect_ratio pad \
     --bf16 True \
-    --output_dir /raid_sdd/whz/experiments/halle/train/exp3_llava_verifier_minus1_9k_1ep_8bz_3e5 \
+    --output_dir /raid_sdd/whz/experiments/halle/train/exp5_with_weight_attn_llava_verifier_matrix_minus1_9k_1ep_8bz_4e5 \
     --num_train_epochs 1 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 4 \
@@ -20,7 +21,7 @@ deepspeed --include localhost:2,3 --master_port 25431 llava/train/train_verifier
     --save_strategy "steps" \
     --save_steps 200 \
     --save_total_limit 1 \
-    --learning_rate 3e-5 \
+    --learning_rate 4e-5 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
@@ -30,3 +31,11 @@ deepspeed --include localhost:2,3 --master_port 25431 llava/train/train_verifier
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
+
+CUDA_VISIBLE_DEVICES=1 python3 eval/model_controller.py \
+            --model-path $model_path \
+            --model-version llava_verifier \
+            --bf16 True \
+            --gt_file_path /raid_sdd/zzy/data/halle/coco/coco2014/annotations/instances_val2014.json \
+            --image_path /raid_sdd/zzy/data/halle/coco/coco2014/val2014 \
+            --output_folder $model_path/eval
