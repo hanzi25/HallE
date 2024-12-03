952efe6e0d37469509d87f0af68975cd72bc1cd8
zzyking <zangzeyuan@bupt.edu.cn>
2024-12-03 20:42:56 +0800

    Modify requirements & add cuda device order

--- scripts/v1_5/model_verifier_eval.sh
+++ scripts/v1_5/model_verifier_eval.sh
@@ -5,11 +5,11 @@
 #             --image_path /raid_sdd/zzy/data/halle/coco/coco2014/val2014 \
 #             --output_folder /raid_sdd/whz/experiments/halle/inference/llava
 
-CUDA_VISIBLE_DEVICES=3 python3 eval/model_controller.py \
-            --model-path /raid_sdd/zzy/experiments/halle/train/exp5_llava_verifier_matrix_minus1_9k_1ep_4bz_3e5/ \
+CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=3 python3 eval/model_controller.py \
+            --model-path /raid_sdi/home/zzy/experiments/halle/train/exp5_llava_verifier_matrix_minus1_9k_1ep_4bz_3e5/ \
             --model-version llava_verifier \
             --use_verifier False \
             --bf16 True \
-            --gt_file_path /raid_sdd/zzy/data/halle/coco/coco2014/annotations/instances_val2014.json \
-            --image_path /raid_sdd/zzy/data/halle/coco/coco2014/val2014 \
-            --output_folder /raid_sdd/zzy/experiments/halle/train/exp5_llava_verifier_matrix_minus1_9k_1ep_4bz_3e5/eval
+            --gt_file_path /raid_sdi/home/zzy/data/halle/coco/coco2014/annotations/instances_val2014.json \
+            --image_path /raid_sdi/home/zzy/data/halle/coco/coco2014/val2014 \
+            --output_folder /raid_sdi/home/zzy/experiments/halle/train/exp5_llava_verifier_matrix_minus1_9k_1ep_4bz_3e5/eval
