2fb1b6c81ecde7f03f4e7a7ecece3be3ff6a1df4
zzyking <zangzeyuan@bupt.edu.cn>
2024-12-18 14:37:28 +0800

    Add extract MSCOCO objects feature

--- scripts/v1_5/model_verifier_eval.sh
+++ scripts/v1_5/model_verifier_eval.sh
@@ -12,4 +12,4 @@ CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=5 python3 eval/model_controlle
             --use_verifier \
             --gt_file_path /raid_sdi/home/zzy/data/halle/coco/coco2014/annotations/instances_val2014.json \
             --image_path /raid_sdi/home/zzy/data/halle/coco/coco2014/val2014 \
-            --output_folder /raid_sdi/home/zzy/experiments/halle/train/exp5_llava_verifier_scalar_init_0.1_minus1_9k_1ep_4bz_3e5/eval
+            --output_folder /raid_sdi/home/zzy/experiments/halle/train/exp5_llava_verifier_scalar_init_0.1_minus1_9k_1ep_4bz_3e5/eval/1212
