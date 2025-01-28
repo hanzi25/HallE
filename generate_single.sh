6ad39f527a6712292c7f75eb1cd578d16cb4d7dc
zzyking <zangzeyuan@bupt.edu.cn>
2025-01-28 21:56:23 +0800

    Manage file directory

--- generate_single.sh
+++ generate_single.sh
@@ -1,4 +1,4 @@
-CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=4 python eval/generate_single.py \
+CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=4 python generate_single.py \
     --model-path /raid_sdi/home/zzy/experiments/halle/train/exp5_llava_verifier_scalar_init_0.1_minus1_9k_1ep_4bz_3e5 \
     --model-version llava_verifier \
     --bf16 \
