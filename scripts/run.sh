7559dc8aef24a61d2014cd7651a1fcb739a849c7
zzyking <zangzeyuan@bupt.edu.cn>
2025-02-25 23:13:59 +0800

    specify versions of dependencies

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 pip3 install -e ".[train,cu12]"
 pip3 install ninja
-pip3 install flash-attn --no-build-isolation
-python3 dl_nltk.py
+pip3 install flash-attn==2.7.0.post2 --no-build-isolation
+python3 utils/dl_nltk.py
