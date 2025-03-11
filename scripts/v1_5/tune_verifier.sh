e46292d3114879678b397cbd59312c47a639d467
zzyking <zangzeyuan@bupt.edu.cn>
2025-03-11 15:45:49 +0000

    update scripts

--- scripts/v1_5/tune_verifier.sh
+++ scripts/v1_5/tune_verifier.sh
@@ -1,8 +1,8 @@
-deepspeed --include localhost:3 --master_port 25432 llava/train/train_verifier.py \
+deepspeed --include localhost:0 --master_port 25436 llava/train/train_verifier.py \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path /raid_sdd/zzy/model/llava_1_5 \
     --version v1 \
-    --data_path /raid_sdd/zzy/data/halle/detail_switch_minus_1_9093.json \
+    --data_path /raid_sdd/zzy/data/halle/sharegpt4v_instruct_gpt4-vision_part_coco_50k.json \
     --image_folder /raid_sdd/zzy/data/halle/coco/train2017 \
     --vision_tower /raid_sdd/zzy/model/clip_vit_large_patch14_336 \
     --mm_projector_type mlp2x_gelu \
@@ -12,16 +12,16 @@ deepspeed --include localhost:3 --master_port 25432 llava/train/train_verifier.p
     --alpha_type scalar \
     --image_aspect_ratio pad \
     --bf16 True \
-    --output_dir /raid_sdd/zzy/experiments/halle/train/exp5_llava_verifier_scalar_init_0.1_minus1_9k_3ep_16bz_3e4 \
-    --num_train_epochs 3 \
-    --per_device_train_batch_size 16 \
+    --output_dir /raid_sdd/zzy/experiments/halle/train/exp7_llava_verifier_scalar_init_0.1_sharegpt_50k_1ep_8bz_3e5 \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 200 \
     --save_total_limit 1 \
-    --learning_rate 3e-4 \
+    --learning_rate 3e-5 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
@@ -30,12 +30,4 @@ deepspeed --include localhost:3 --master_port 25432 llava/train/train_verifier.p
     --model_max_length 2048 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
-    --lazy_preprocess True \
-
-#CUDA_VISIBLE_DEVICES=1 python3 eval/model_controller.py \
-#            --model-path $model_path \
-#            --model-version llava_verifier \
-#            --bf16 True \
-#            --gt_file_path /raid_sdd/zzy/data/halle/coco/coco2014/annotations/instances_val2014.json \
-#            --image_path /raid_sdd/zzy/data/halle/coco/coco2014/val2014 \
-#            --output_folder $model_path/eval
+    --lazy_preprocess True
