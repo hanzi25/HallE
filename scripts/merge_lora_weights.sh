6f382cec47b6970168ec646748f4baf9620d232b
zzyking <zangzeyuan@bupt.edu.cn>
2025-09-17 08:09:26 +0000

    relocate merge_lora_weights to scripts/

--- scripts/merge_lora_weights.sh
+++ scripts/merge_lora_weights.sh
@@ -1,7 +1,7 @@
 MODEL_PATH=/raid_sdd/zzy/experiments/halle/train/exp14_llava_lora_joint_6+3+1+1.5k_layer32_1ep_16bz_3e5
 MODEL_BASE=/raid_sdd/zzy/model/llava_1_5
 
-CUDA_VISIBLE_DEVICES=3 python eval/merge_lora_weights.py \
+CUDA_VISIBLE_DEVICES=3 python scripts/merge_lora_weights.py \
     --model-path ${MODEL_PATH} \
     --model-base ${MODEL_BASE} \
     --save-model-path ${MODEL_PATH}_merged \
